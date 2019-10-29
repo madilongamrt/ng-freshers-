fd70976f800b93c67f1c41db091e63957be66c68
Rudzani Matshothsi <rudzani@thedigitalacademy.co.za>
2019-10-29 16:59:38 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
+import { PersonIputComponent } from './persons/persona-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsComponent, PersonIputComponent
   ],
   imports: [
     BrowserModule
